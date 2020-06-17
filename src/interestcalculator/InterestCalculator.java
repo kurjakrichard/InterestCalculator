d5a31a0b74cb48e311b5805ad570c31bfdfede1e
Kurják Richárd <kurjak.richard@gmail.com>
2020-06-18 01:56:54 +0200

    Create javafx application

---
+++ src/interestcalculator/InterestCalculator.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interestcalculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author balza
+ */
+public class InterestCalculator extends Application{
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("interestFXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("PhoneBook");
+        stage.setScene(scene);
+        stage.show(); 
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/interestcalculator/InterestFXMLController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interestcalculator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author balza
+ */
+public class InterestFXMLController implements Initializable {
+
+    @FXML
+    private SplitPane mainSplit;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
